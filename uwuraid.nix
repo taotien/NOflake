175f67e377fae90b07959a1cac27903e62858802
Tao Tien <29749622+taotien@users.noreply.github.com>
2023-07-22 17:56:50 -0700

    fix server mounts

--- uwuraid.nix
+++ uwuraid.nix
@@ -5,10 +5,10 @@
       (commonMountOptions // { what = "100.86.160.104:/mnt/user/anime"; where = "/mnt/uwuraid/anime"; })
       (commonMountOptions // { what = "100.86.160.104:/mnt/user/backup"; where = "/mnt/uwuraid/backup"; })
       (commonMountOptions // { what = "100.86.160.104:/mnt/user/everything"; where = "/mnt/uwuraid/everything"; })
-      (commonMountOptions // { what = "100.86.160.104:/mnt/user/movies"; where = "/mnt/uwuraid/syncthing"; })
-      (commonMountOptions // { what = "100.86.160.104:/mnt/user/photos"; where = "/mnt/uwuraid/television"; })
-      (commonMountOptions // { what = "100.86.160.104:/mnt/user/syncthing"; where = "/mnt/uwuraid/photos"; })
-      (commonMountOptions // { what = "100.86.160.104:/mnt/user/television"; where = "/mnt/uwuraid/movies"; })
+      (commonMountOptions // { what = "100.86.160.104:/mnt/user/movies"; where = "/mnt/uwuraid/movies"; })
+      (commonMountOptions // { what = "100.86.160.104:/mnt/user/photos"; where = "/mnt/uwuraid/photos"; })
+      (commonMountOptions // { what = "100.86.160.104:/mnt/user/syncthing"; where = "/mnt/uwuraid/syncthing"; })
+      (commonMountOptions // { what = "100.86.160.104:/mnt/user/television"; where = "/mnt/uwuraid/television"; })
     ];
   systemd.automounts = let commonAutoMountOptions = { wantedBy = [ "multi-user.target" ]; automountConfig = { TimeoutIdleSec = "60"; }; }; in
     [
