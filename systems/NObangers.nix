c42b395f50b83f5479a0b38c9b49eb8e979818be
Tao Tien <>
2023-09-30 23:35:09 -0700

    fix hifiberry audio

--- systems/NObangers.nix
+++ systems/NObangers.nix
@@ -15,24 +15,47 @@
   services.printing.enable = false;
   services.btrfs.autoScrub.enable = false;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-  # console.enable = false;
-  # powerManagement.cpuFreqGovernor = "ondemand";
+  boot = {
+    # initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    initrd.availableKernelModules = [ "xhci_pci" ];
+    kernelParams = [
+      "8250.nr_uarts=1"
+      "cma=128M"
+      "console=tty1"
+      # "console=ttyAMA0,115200"
+    ];
+    loader = {
+      raspberryPi = {
+        enable = true;
+        version = 4;
+      };
+      systemd-boot.enable = false;
+      generic-extlinux-compatible.enable = false;
+    };
+    kernelPackages = pkgs.linuxPackages_rpi4;
+  };
+
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   hardware = {
     raspberry-pi."4" = {
-      # apply-overlays-dtmerge.enable = true;
-      # audio.enable = true;
+      apply-overlays-dtmerge.enable = true;
+      audio.enable = true;
       fkms-3d.enable = true;
     };
-    # deviceTree = {
-    # enable = true;
-    # filter = "*rpi-4-*.dtb";
-    # };
+    deviceTree = {
+      enable = true;
+      filter = "*rpi-4-*.dtb";
+      ]
+    };
   };
+  hardware.deviceTree.overlays = [{
+    name = hifiberry-dacplusadc";
+    dtsText = ''
+
+    ''
+  }
+  ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
   system.stateVersion = "23.11";
