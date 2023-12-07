1ff68fa7ef7726e563094f4e78058172595a3aba
Tao Tien <29749622+taotien@users.noreply.github.com>
2023-12-06 16:00:59 -0800

    begin server

---
+++ systems/NOserver.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    bat
+    bottom
+    cifs-utils
+    git
+    unstable.helix
+    ouch
+    ripgrep
+    skim
+    tree
+    wget
+  ];
+
+  programs.partition-manager.enable = false;
+  services.flatpak.enable = false;
+  services.printing.enable = false;
+  hardware.bluetooth.enable = false;
+  services.xserver.enable = false;
+  services.xserver.displayManager.sddm.enable = false;
+  services.xserver.desktopManager.plasma5.enable = false;
+  services.pipewire.enable = false;
+
+  environment.variables = {
+    EDITOR = "hx";
+    VISUAL = "hx";
+    SKIM_DEFAULT_COMMAND = "rg --files";
+  };
+
+  environment.sessionVariables = {
+    EDITOR = "hx";
+    VISUAL = "hx";
+    SKIM_DEFAULT_COMMAND = "rg --files";
+  };
+
