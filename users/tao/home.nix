14912f98c56c82ff532129924f18150449504f0c
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-01 08:56:18 -0800

    nu et al

--- users/tao/home.nix
+++ users/tao/home.nix
@@ -3,15 +3,40 @@
     ./helix.nix
   ];
 
-  programs.wezterm = {
+  programs.nushell = {
     enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
+    configFile.source = ./config.nu;
+    envFile.source = ./env.nu;
   };
 
   programs.git = {
     enable = true;
     userName = "Tao Tien";
     userEmail = "29749622+taotien@users.noreply.github.com";
+  };
+
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+    settings = {
+      nix_shell = {
+        disabled = false;
+        impure_msg = ";[impure shell](bold red)";
+        pure_msg = ";[pure shell](bold green)";
+        unknown_msg = ";[unknown shell](bold yellow)";
+        format = "via [☃️ $state( \($name\))](bold blue)";
+      };
+    };
+  };
+
+  programs.wezterm = {
+    enable = true;
+    extraConfig = builtins.readFile ./wezterm.lua;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
   };
 
   home.username = "tao";
