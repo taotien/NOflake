117650f17ec0beb8a9c740d2426fa9344dc2ba55
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-08 10:37:39 -0800

    cosmic-term

---
+++ extras/cosmic.nix
@@ -0,0 +1,17 @@
+< < <<<<<
++++++++
+%%%%%%%
+{ pkgs, ... }: {
+environment.systemPackages = with pkgs; [
+cosmic-term
+fontconfig
+freetype
+lld
+pkg-config
+];
+
+# environment.variables = {
+#   PKG_CONFIG_PATH =
+#     };
+}
+>>>>>>>
