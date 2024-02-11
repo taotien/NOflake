5d98b5e3fdb5b8626d8d2fb0058b75a1a097debc
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-10 19:16:29 -0800

    cosmic de testing
    what

--- extras/cosmic.nix
+++ extras/cosmic.nix
@@ -1,17 +1,13 @@
-< < <<<<<
-+++++++
-%%%%%%%
 { pkgs, ... }: {
-environment.systemPackages = with pkgs; [
-cosmic-term
-fontconfig
-freetype
-lld
-pkg-config
-];
+  environment.systemPackages = with pkgs; [
+    cosmic-term
+    fontconfig
+    freetype
+    lld
+    pkg-config
+  ];
 
-# environment.variables = {
-#   PKG_CONFIG_PATH =
-#     };
+  # environment.variables = {
+  #   PKG_CONFIG_PATH =
+  #     };
 }
->>>>>>>
