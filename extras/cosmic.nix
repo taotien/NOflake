274b98a533b13df223e24e6730b8976b169758ef
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-26 16:55:21 -0800

    cosmic all the things

--- extras/cosmic.nix
+++ extras/cosmic.nix
@@ -1,13 +1,25 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    cosmic-bg
+    cosmic-applets
+    cosmic-applibrary
+    cosmic-comp
+    cosmic-edit
+    cosmic-files
+    cosmic-greeter
+    cosmic-icons
+    cosmic-launcher
+    cosmic-notifications
+    cosmic-osd
+    cosmic-panel
+    cosmic-protocols
+    cosmic-randr
+    cosmic-screenshot
+    cosmic-session
+    cosmic-settings
+    cosmic-settings-daemon
     cosmic-term
-    fontconfig
-    freetype
-    lld
-    pkg-config
+    cosmic-workspaces-epoch
+    xdg-desktop-portal-cosmic
   ];
-
-  # environment.variables = {
-  #   PKG_CONFIG_PATH =
-  #     };
 }
