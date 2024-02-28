bb1cae8a9caf4808485f2f684423949813a7e0a2
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-27 20:09:11 -0800

    use sddm for now

--- extras/cosmic.nix
+++ extras/cosmic.nix
@@ -1,30 +1,29 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    cosmic-bg
-    cosmic-applets
-    cosmic-applibrary
-    cosmic-comp
-    cosmic-edit
-    cosmic-files
-    cosmic-greeter
-    cosmic-icons
-    cosmic-launcher
-    cosmic-notifications
-    cosmic-osd
-    cosmic-panel
-    cosmic-protocols
-    cosmic-randr
-    cosmic-screenshot
-    cosmic-session
-    cosmic-settings
-    cosmic-settings-daemon
-    cosmic-term
-    cosmic-workspaces-epoch
-    xdg-desktop-portal-cosmic
-  ];
-  services.xserver.displayManager.cosmic-greeter.enable = true;
+  # environment.systemPackages = with pkgs; [
+  #   cosmic-bg
+  #   cosmic-applets
+  #   cosmic-applibrary
+  #   cosmic-comp
+  #   cosmic-edit
+  #   cosmic-files
+  #   cosmic-greeter
+  #   cosmic-icons
+  #   cosmic-launcher
+  #   cosmic-notifications
+  #   cosmic-osd
+  #   cosmic-panel
+  #   cosmic-protocols
+  #   cosmic-randr
+  #   cosmic-screenshot
+  #   cosmic-session
+  #   cosmic-settings
+  #   cosmic-settings-daemon
+  #   cosmic-term
+  #   cosmic-workspaces-epoch
+  #   xdg-desktop-portal-cosmic
+  # ];
+  services.xserver.displayManager.cosmic-greeter.enable = false;
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.cosmic.enable = true;
-  services.pipewire = {
-    enable = false;
-  };
+  services.xserver.desktopManager.plasma5.enable = true;
 }
