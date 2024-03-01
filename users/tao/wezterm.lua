a9a47ac33f35f06b8aef46401043cd86626f0aa1
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-02-29 22:14:53 -0800

    allow warn

--- users/tao/wezterm.lua
+++ users/tao/wezterm.lua
@@ -1,7 +1,7 @@
 return {
 	font = wezterm.font 'FiraCode Nerd Font',
 	color_scheme = "Dracula",
-	warn_about_missing_glyphs = false,
+	warn_about_missing_glyphs = true,
 	check_for_updates = false,
 
 	hide_tab_bar_if_only_one_tab = true,
