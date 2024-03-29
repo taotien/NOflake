bea970378ea267cc7f0137672806811c0b8725bb
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-03-29 12:51:49 -0700

    vy

--- users/vy.nix
+++ users/vy.nix
@@ -11,6 +11,18 @@
   ];
   programs.kdeconnect.enable = true;
 
+  services.snapper.configs = {
+    home = {
+      SUBVOLUME = "/home";
+      ALLOW_USERS = ["tao"];
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = "5";
+      TIMELINE_LIMIT_DAILY = "7";
+    };
+  };
+  services.snapper.snapshotInterval = "*:0/5";
+
   fonts.packages = with pkgs; [
     noto-fonts-cjk
     noto-fonts-color-emoji
