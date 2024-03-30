ef3e943d3ee2c5327171e15e747848b76f903648
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-03-30 18:35:45 -0500

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   NOlaptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFACYTHNr0dgtTe8cb6q+NwI1KaKJmNsUrnz5/8ZDvH";
-  systems = [NOlaptop];
+  NOmom = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdPvIo0P00I27i9XQFngsklw/dSyoFs7EgRt7RvkbSq";
+  systems = [NOlaptop NOmom];
 in {
   "uwuraid.age".publicKeys = systems;
   "syncthing-NOcomputer.age".publicKeys = systems;
