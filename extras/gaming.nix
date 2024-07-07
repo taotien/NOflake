564438fd7c23d3e087c59ecc3df75b2206186350
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-07-07 00:11:03 -0700

    backout of commit d625fa85567268df8c73c2cc8ed7296ecea45904

--- extras/gaming.nix
+++ extras/gaming.nix
@@ -56,48 +56,48 @@ in {
   ];
 
   # THE FINALS audio borked
-  # services.pipewire.enable = !if_desktop;
-  # hardware.pulseaudio.enable = if_desktop;
-  # hardware.pulseaudio.support32Bit = if_desktop;
-  # services.pipewire.extraConfig = {
-  #   pipewire."99-low-latency" = {
-  #     context.properties = {
-  #       default.allowed-rates = [44100 48000 96000];
-  #       default.clock.rate = 192000;
-  #       default.clock.quantum = 32;
-  #       default.clock.min-quantum = 32;
-  #       default.clock.max-quantum = 32;
-  #     };
-  #     context.modules = [
-  #       {
-  #         name = "libpipewire-module-rt";
-  #         args = {
-  #           nice.level = -12;
-  #           rt.prio = 89;
-  #           rt.time.soft = 200000;
-  #           rt.time.hard = 200000;
-  #         };
-  #         flags = ["ifexists nofail"];
-  #       }
-  #     ];
-  #   };
-  #   pipewire-pulse."99-low-latency" = {
-  #     context.modules = [
-  #       {
-  #         name = "libpipewire-module-protocol-pulse";
-  #         args = {
-  #           pulse.min.req = "32/192000";
-  #           pulse.default.req = "32/192000";
-  #           pulse.max.req = "32/192000";
-  #           pulse.min.quantum = "32/192000";
-  #           pulse.max.quantum = "32/192000";
-  #         };
-  #       }
-  #     ];
-  #     stream.properties = {
-  #       node.latency = "32/192000";
-  #       resample.quality = 1;
-  #     };
-  #   };
-  # };
+  services.pipewire.enable = !if_desktop;
+  hardware.pulseaudio.enable = if_desktop;
+  hardware.pulseaudio.support32Bit = if_desktop;
+  services.pipewire.extraConfig = {
+    pipewire."99-low-latency" = {
+      context.properties = {
+        default.allowed-rates = [44100 48000 96000];
+        default.clock.rate = 192000;
+        default.clock.quantum = 32;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 32;
+      };
+      context.modules = [
+        {
+          name = "libpipewire-module-rt";
+          args = {
+            nice.level = -12;
+            rt.prio = 89;
+            rt.time.soft = 200000;
+            rt.time.hard = 200000;
+          };
+          flags = ["ifexists nofail"];
+        }
+      ];
+    };
+    pipewire-pulse."99-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/192000";
+            pulse.default.req = "32/192000";
+            pulse.max.req = "32/192000";
+            pulse.min.quantum = "32/192000";
+            pulse.max.quantum = "32/192000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/192000";
+        resample.quality = 1;
+      };
+    };
+  };
 }
