19a7a58e24d76ad7b7a645e7f53fba9cf72d41a4
Tao Tien <29749622+taotien@users.noreply.github.com>
2025-02-18 00:45:57 -0800

    rekey & bump nushell conf

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # get these from /etc/ssh
   NOcomputer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPv53GM1uDDOdRxIlHmpf6x2y13yT5bFDNyrgDGLAR1l";
-  NOlaptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJr8RRPHu9MPa7oNfPbCJK3Lh/FL+WRiaZrI+1JVXBNI";
+  NOlaptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtdTREsBxzg/8s0lVCwL+r18qMP8glxAKaKEg+71I6m";
   NOmom = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdPvIo0P00I27i9XQFngsklw/dSyoFs7EgRt7RvkbSq";
   systems = [NOcomputer NOlaptop NOmom];
 in {
