92abf9126369756d8357a8f2e0550d9b64cec94b
Tao Tien <29749622+taotien@users.noreply.github.com>
2025-04-29 23:08:22 -0700

    switch to nixpkgs cosmic?

--- extras/cosmic.nix
+++ extras/cosmic.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
-  services.displayManager.sddm.enable = true;
-  services.displayManager.cosmic-greeter.enable = false;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.cosmic-greeter.enable = false;
   services.desktopManager.cosmic.enable = true;
 
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
