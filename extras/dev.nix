69c57dc9d20041871e89a27177d7195ff21c2906
Tao Tien <29749622+taotien@users.noreply.github.com>
2025-05-23 16:45:21 -0700

    oxi

--- extras/dev.nix
+++ extras/dev.nix
@@ -1,118 +1,114 @@
 {pkgs, ...}: {
-    # services.globalprotect = {
-    #   enable = true;
-    # settings = {
-    #   "vpn1.usfca.edu" = {
-    #     openconnect-args = "";
-    #   };
-    # };
-    # };
+  environment.systemPackages = with pkgs; [
+    delta
+    mask
+    mprocs
+    presenterm
+    # (llm.withPlugins (ps: with ps; [llm-openrouter]))
+    # bash-language-server
+    # digital
+    # etcher
+    # fontforge
+    # freecad
+    # llm
+    # lychee # link checker
+    # quickemu
+    # radicle-node
+    # vulkan-loader
+    # act
+    # aichat
+    # aider-chat
+    # asm-lsp
+    b3sum
+    devenv
+    direnv
+    gh
+    # git-cliff
+    hyperfine
+    jujutsu
+    just
+    lldb
+    # lua-language-server
+    # markdown-oxide # markdown lsp
+    qFlipper
+    qemu
+    sd
+    sshfs
+    taplo # toml lsp
+    tio
+    tokei # repo code stats
+    typos
 
-    environment.systemPackages = with pkgs; [
-        # (llm.withPlugins (ps: with ps; [llm-openrouter]))
-        # bash-language-server
-        # digital
-        # etcher
-        # fontforge
-        # freecad
-        # llm
-        # lychee # link checker
-        # quickemu
-        # radicle-node
-        # vulkan-loader
-        # act
-        # aichat
-        # aider-chat
-        # asm-lsp
-        b3sum
-        devenv
-        direnv
-        gh
-        # git-cliff
-        hyperfine
-        jujutsu
-        just
-        lldb
-        # lua-language-server
-        # markdown-oxide # markdown lsp
-        qFlipper
-        qemu
-        sd
-        sshfs
-        taplo # toml lsp
-        tio
-        tokei # repo code stats
-        typos
+    # # databases
+    # sqlite
+    # sqls
+    # sqlx-cli
+    # sea-orm-cli
+    # rainfrog
 
-        # # databases
-        # sqlite
-        # sqls
-        # sqlx-cli
-        # sea-orm-cli
-        # rainfrog
+    # # arduino
+    # arduino-language-server
+    # arduino-cli
+    # pkgsCross.avr.buildPackages.gcc
+    # avrdude
+    # ravedude
 
-        # # arduino
-        # arduino-language-server
-        # arduino-cli
-        # pkgsCross.avr.buildPackages.gcc
-        # avrdude
-        # ravedude
+    # c
+    clang
+    clang-tools
+    gcc
+    gdb
+    gnumake
 
-        # c
-        clang
-        clang-tools
-        gcc
-        gdb
-        gnumake
+    # nix
+    nil
+    alejandra
 
-        # nix
-        nil
-        alejandra
+    # python
+    # pypy3
+    python3
+    # (python3.withPackages (ps:
+    #   with ps; [
+    #     llm
+    #     # llm-openrouter
+    #   ]))
+    # python310Packages.python-lsp-server
+    uv
+    ruff
 
-        # python
-        # pypy3
-        python3
-        # (python3.withPackages (ps:
-        #   with ps; [
-        #     llm
-        #     # llm-openrouter
-        #   ]))
-        # python310Packages.python-lsp-server
-        uv
-        ruff
+    # rust
+    cargo-binstall
+    bacon
+    cargo-expand
+    cargo-feature
+    cargo-generate
+    cargo-watch
+    elf2uf2-rs
+    flip-link
+    mdbook
+    mold-wrapped
+    probe-rs
+    rustup
+    rusty-man
+    sccache
+    trunk
+    wasm-bindgen-cli
+  ];
 
-        # rust
-        cargo-binstall
-        bacon
-        cargo-expand
-        cargo-feature
-        cargo-generate
-        cargo-watch
-        elf2uf2-rs
-        flip-link
-        mdbook
-        mold-wrapped
-        probe-rs
-        rustup
-        sccache
-        trunk
-        wasm-bindgen-cli
-    ];
+  hardware.flipperzero.enable = true;
 
-    hardware.flipperzero.enable = true;
+  services.udev.extraRules = ''
+    SUBSYSTEM == "tty", GROUP="dialout", ATTRS{interface}=="Black Magic GDB Server", SYMLINK+="ttyBmpGdb"
+    SUBSYSTEM == "tty", GROUP="dialout", ATTRS{interface}=="Black Magic UART Port",  SYMLINK+="ttyBmpTarg"
+  '';
 
-    services.udev.extraRules = ''
-        SUBSYSTEM == "tty", GROUP="dialout", ATTRS{interface}=="Black Magic GDB Server", SYMLINK+="ttyBmpGdb"
-        SUBSYSTEM == "tty", GROUP="dialout", ATTRS{interface}=="Black Magic UART Port",  SYMLINK+="ttyBmpTarg"
-    '';
-
-    # virtualisation.docker = {
-    #   enable = true;
-    #   storageDriver = "btrfs";
-    # };
-    # virtualisation.docker.rootless = {
-    #   enable = true;
-    #   setSocketVariable = true;
-    # };
-    # users.extraGroups.docker.members = ["tao"];
+  # virtualisation.docker = {
+  #   enable = true;
+  #   storageDriver = "btrfs";
+  # };
+  # virtualisation.docker.rootless = {
+  #   enable = true;
+  #   setSocketVariable = true;
+  # };
+  # users.extraGroups.docker.members = ["tao"];
 }
